8b3ba8ea7834e0a673795f667d71867ea67802b8
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-06 19:11:44 +0300

    Adding if statement to print empty list when length=zero

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length == 0:
+        print([])
+        return
+    
     x, y = 0, 1
     for _ in range(length): #Used the variable (_) after "for" as a placeholder for the loop variable, indicatting that it isn't going to be used.
         print(x)
         x, y = y, x + y
 
 print_fibonacci(15)
+
