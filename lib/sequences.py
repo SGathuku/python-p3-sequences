00b1d4fcd1a0a65e9955bf70959d57e7a0bbba8a
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-06 19:24:42 +0300

    Adding while loop to generate the remaining numbers in Fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    if length == 0:
-        print([])
-        return
-    
-    x, y = 0, 1
-    for _ in range(length): #Used the variable (_) after "for" as a placeholder for the loop variable, indicatting that it isn't going to be used.
-        print(x)
-        x, y = y, x + y
+    fibo_sequence = [0, 1]
+
+    while len(fibo_sequence) < length:
+        next_number = fibo_sequence[-1] + fibo_sequence[-2]
+        fibo_sequence.append(next_number)
+
+    for num in fibo_sequence:
+        print(num)
 
 print_fibonacci(15)
-
