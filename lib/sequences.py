4153e195040d67d563450e0699f1efb9de0584d7
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-06 19:26:28 +0300

    Adding For_in loop to iterate over each number in fibo_sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -7,7 +7,7 @@ def print_fibonacci(length):
         next_number = fibo_sequence[-1] + fibo_sequence[-2]
         fibo_sequence.append(next_number)
 
-    for num in fibo_sequence:
-        print(num)
+    for number in fibo_sequence:
+        print(number)
 
 print_fibonacci(15)
