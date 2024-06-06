bc0e8aa1adfd9823f8414f1ccbbd19552b97e2c3
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-06 19:38:33 +0300

    Adding If_elif_else statement

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    fibo_sequence = [0, 1]
-
-    while len(fibo_sequence) < length:
-        next_number = fibo_sequence[-1] + fibo_sequence[-2]
-        fibo_sequence.append(next_number)
-
-    print(fibo_sequence)
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fibo_sequence = [0, 1]
+        
+        while len(fibo_sequence) < length:
+            next_number = fibo_sequence[-1] + fibo_sequence[-2]
+            fibo_sequence.append(next_number)
+            
+        print(fibo_sequence)
