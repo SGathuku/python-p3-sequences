b7a28617905b3d97497c11e5b3cdcb12af03e9ba
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-06 19:32:12 +0300

    Removing For_loop

--- lib/sequences.py
+++ lib/sequences.py
@@ -7,7 +7,4 @@ def print_fibonacci(length):
         next_number = fibo_sequence[-1] + fibo_sequence[-2]
         fibo_sequence.append(next_number)
 
-    for number in fibo_sequence:
-        print(number)
-
-print_fibonacci(15)
+    print(fibo_sequence)
